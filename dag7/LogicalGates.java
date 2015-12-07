a27aff940ea635313fadb97d18ddb7a8abbd74be
Jakob Svemar <jakob_svemar@hotmail.com>
2015-12-07 15:23:56 +0100

    refactored some, still doesn't work

--- dag7/LogicalGates.java
+++ dag7/LogicalGates.java
@@ -13,25 +13,25 @@ public class LogicalGates{
     public void add (String line){
         String[] s = line.split(" ");
         if(line.contains("AND")){
-            instructions.put(s[4], new And(s[0], s[2]));
+            instructions.put(s[4], new And(s[0], s[2], s[4]));
             
         } else if (line.contains("OR")) {
-            instructions.put(s[4], new Or(s[0], s[2]));
+            instructions.put(s[4], new Or(s[0], s[2], s[4]));
             
         } else if (line.contains("NOT")){
-            instructions.put(s[3], new Not(s[1]));
+            instructions.put(s[3], new Not(s[1], s[3]));
 
             
         } else if (line.contains("LSHIFT")){
-            instructions.put(s[4], new LShift(s[0], s[2]));
+            instructions.put(s[4], new LShift(s[0], s[2], s[4]));
 
             
         } else if (line.contains("RSHIFT")){
-            instructions.put(s[4], new RShift(s[0], s[2]));
+            instructions.put(s[4], new RShift(s[0], s[2], s[4]));
 
             
         } else { //normal input
-            instructions.put(s[2], new Input(s[0]));
+            instructions.put(s[2], new Input(s[0], s[2]));
         }
     }
 
@@ -49,155 +49,118 @@ public class LogicalGates{
             lg.add(line);
             line = br.readLine();
         }
-        System.out.println("File read, processing");
-        System.out.println(lg.getWire(args[1]));
+         System.out.println(lg.getWire(args[1]));
     }
     
     
     
     
     private class Instruction{
-        String inputA;
+        protected String iAm;
+        protected String inputA;
         
-        public Instruction(String inputA){
+        public Instruction(String inputA, String iAm){
             this.inputA = inputA;
+            this.iAm = iAm;
         }
         
         public int run(){
             return 0;
         }
+        
+        public int calculate(String s){
+            if(s.matches("\\d+")){
+                return Integer.parseInt(s);
+            } else {
+                int i = instructions.get(s).run();
+                instructions.put(iAm, new Input(Integer.toString(i), iAm));
+                return i;
+            }
+        }
     }
                             
     private class Input extends Instruction{
-        public Input(String input){
-            super(input);
+        public Input(String input, String iAm){
+            super(input, iAm);
         }
         
         @Override
         public int run(){
-            return Integer.parseInt(inputA);
+            System.out.println("INPUT " + inputA + " to " + iAm);
+            return calculate(inputA);
         }
     }
     
     private class And extends Instruction {
-        String inputB;
+        private String inputB;
         
-        public And(String a, String b){
-            super(a);
+        public And(String a, String b, String iAm){
+            super(a, iAm);
             this.inputB = b;
         }
         
         @Override
         public int run(){
-            int a;
-            int b;
-            if(inputA.matches("\\d+")){
-                a = Integer.parseInt(inputA);
-            } else {
-                a = instructions.get(inputA).run();
-            }
-            if(inputB.matches("\\d+")){
-                b = Integer.parseInt(inputB);
-            }else {
-                b = instructions.get(inputB).run();
-            }
-            return (int)(a & b);
+            System.out.println(inputA + " AND " + inputB + " to " + iAm);
+            return calculate(inputA) & calculate(inputB);
         }
     }
 
     private class LShift extends Instruction {
-        String inputB;
+        private String inputB;
         
-        public LShift(String a, String b){
-            super(a);
+        public LShift(String a, String b, String iAm){
+            super(a, iAm);
             this.inputB = b;
         }
         
         @Override
         public int run(){
-            int a;
-            int b;
-            if(inputA.matches("\\d+")){
-                a = Integer.parseInt(inputA);
-            } else {
-                a = instructions.get(inputA).run();
-            }
-            if(inputB.matches("\\d+")){
-                b = Integer.parseInt(inputB);
-            }else {
-                b = instructions.get(inputB).run();
-            }
-            return (int)(a << b);
+            System.out.println(inputA + " LSHIFT " + inputB + " to " + iAm);
+            return (calculate(inputA) << calculate(inputB)) & 0xFFFF;
         }
     }
     
     private class Not extends Instruction {
         
-        public Not(String a){
-            super(a);
+        public Not(String a, String iAm){
+            super(a, iAm);
         }
         
         @Override
         public int run(){
-            int a;
-            if(inputA.matches("\\d+")){
-                a = Integer.parseInt(inputA);
-            } else {
-                a = instructions.get(inputA).run();
-            }
-            return (int)(~a);
+            System.out.println("NOT " + inputA + " to " + iAm);
+            return (~calculate(inputA)) & 0xFFFF;
         }
     }
     
     private class Or extends Instruction {
-        String inputB;
+        private String inputB;
         
-        public Or(String a, String b){
-            super(a);
+        public Or(String a, String b, String iAm){
+            super(a, iAm);
             this.inputB = b;
         }
         
         @Override
         public int run(){
-            int a;
-            int b;
-            if(inputA.matches("\\d+")){
-                a = Integer.parseInt(inputA);
-            } else {
-                a = instructions.get(inputA).run();
-            }
-            if(inputB.matches("\\d+")){
-                b = Integer.parseInt(inputB);
-            }else {
-                b = instructions.get(inputB).run();
-            }
-            return (int)(a | b);
+            System.out.println(inputA + " OR " + inputB + " to " + iAm);
+            return calculate(inputA) | calculate(inputB);
         }
     }
 
     private class RShift extends Instruction {
-        String inputB;
+        private String inputB;
         
-        public RShift(String a, String b){
-            super(a);
+        public RShift(String a, String b, String iAm){
+            super(a, iAm);
             this.inputB = b;
         }
         
         @Override
         public int run(){
-            int a;
-            int b;
-            if(inputA.matches("\\d+")){
-                a = Integer.parseInt(inputA);
-            } else {
-                a = instructions.get(inputA).run();
-            }
-            if(inputB.matches("\\d+")){
-                b = Integer.parseInt(inputB);
-            }else {
-                b = instructions.get(inputB).run();
-            }
-            return (int)(a >> b);
+            System.out.println(inputA + " RSHIFT " + inputB + " to " + iAm);
+            return calculate(inputA) >> calculate(inputB);
         }
     }
 
